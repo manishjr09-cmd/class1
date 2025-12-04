5d2059f5dd01b1b36cc9053d7a3644dc00e37903
manishjr09-cmd <manishjr09@gmail.com>
2025-12-04 15:37:19 +0530

    datatypes

---
+++ datatypes.java
@@ -0,0 +1,18 @@
+package basicjava;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //int
+		int ageofPerson=30;
+		
+		char logo='t';
+		
+		String name="MANISH KUMAR";
+		
+		boolean value=true;
+		System.out.println(name);
+	}
+
+}
