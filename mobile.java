69dac008ec50d2720863f0dd94391aa92c514de7
manishjr09-cmd <manishjr09@gmail.com>
2025-12-05 10:36:54 +0530

    Mobile

---
+++ mobile.java
@@ -0,0 +1,16 @@
+
+
+public class mobile {
+   
+   public void takevideo() {
+	   System.out.println("take video");
+	   
+   }
+   
+   public static void main(String[] args) {
+		mobile obj=new mobile();
+		obj.takevideo();
+
+	}
+
+}
